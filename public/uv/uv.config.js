ba21c27ec36b73e8b7969c5619d835286ffdf35e
satoknan-droid <satoknan@gmail.com>
2026-06-20 23:06:03 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,10 @@
 self.__uv$config = {
   prefix: "/uv/",
   bare: "https://tomp.app/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
+  encodeUrl: self.Ultraviolet.codec.xor.encode,
+  decodeUrl: self.Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
   bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
-``
