6c2ac860afb7308205cf3e2add61d3bc4cd04590
satoknan-droid <satoknan@gmail.com>
2026-06-20 22:12:05 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { hostname } from "node:os";
 import { createServer } from "node:http";
 import express from "express";
 import wisp from "wisp-server-node";
@@ -12,7 +11,7 @@ const app = express();
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-    res.sendFile("public/index.html", { root: process.cwd() });
+  res.sendFile("public/index.html", { root: process.cwd() });
 });
 
 app.use("/uv/", express.static(uvPath));
@@ -20,24 +19,24 @@ app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
 
 app.use((req, res) => {
-    res.status(404);
-    res.sendFile("public/404.html", { root: process.cwd() });
+  res.status(404);
+  res.sendFile("public/404.html", { root: process.cwd() });
 });
 
 const server = createServer();
 
 server.on("request", (req, res) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    app(req, res);
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  app(req, res);
 });
 
 server.on("upgrade", (req, socket, head) => {
-    if (req.url.endsWith("/wisp/")) {
-        wisp.routeRequest(req, socket, head);
-        return;
-    }
-    socket.end();
+  if (req.url.endsWith("/wisp/")) {
+    wisp.routeRequest(req, socket, head);
+    return;
+  }
+  socket.end();
 });
 
 const port = process.env.PORT || 8080;
