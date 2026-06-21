bc4ed147a0ce03f643ec4c5f5a016b70643fbce6
satoknan-droid <satoknan@gmail.com>
2026-06-21 13:47:48 +0900

    Update register-sw.js

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,28 +1,24 @@
 "use strict";
-/**
- * Distributed with Ultraviolet and compatible with most configurations.
- */
-const stockSW = "/uv/sw.js";
 
-/**
- * List of hostnames that are allowed to run serviceworkers on http://
- */
+const stockSW = "/uv/uv.sw.js";
+
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
-/**
- * Global util
- * Used in 404.html and index.html
- */
 async function registerSW() {
-	if (!navigator.serviceWorker) {
-		if (
-			location.protocol !== "https:" &&
-			!swAllowedHostnames.includes(location.hostname)
-		)
-			throw new Error("Service workers cannot be registered without https.");
+  if (!navigator.serviceWorker) {
+    if (
+      location.protocol !== "https:" &&
+      !swAllowedHostnames.includes(location.hostname)
+    ) {
+      throw new Error(
+        "Service workers cannot be registered without https."
+      );
+    }
 
-		throw new Error("Your browser doesn't support service workers.");
-	}
+    throw new Error("Your browser doesn't support service workers.");
+  }
 
-	await navigator.serviceWorker.register(stockSW);
+  return await navigator.serviceWorker.register(stockSW, {
+    scope: "/uv/",
+  });
 }
