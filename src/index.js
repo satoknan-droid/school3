ce90d71393b55f851cf4e68975a33e265ac6c1ad
satoknan-droid <satoknan@gmail.com>
2026-06-21 11:23:48 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../public/index.html");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
