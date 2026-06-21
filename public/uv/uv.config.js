d48f2f02892278cf60b5cd851348dd01d042dcb3
satoknan-droid <satoknan@gmail.com>
2026-06-21 13:58:13 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,4 +1,12 @@
+// This file overwrites the stock UV config.js
+
 self.__uv$config = {
-  prefix: "/uv/",
-  bare: "https://tomp.app/",
+	prefix: "/uv/service/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/uv/uv.handler.js",
+	client: "/uv/uv.client.js",
+	bundle: "/uv/uv.bundle.js",
+	config: "/uv/uv.config.js",
+	sw: "/uv/uv.sw.js",
 };
